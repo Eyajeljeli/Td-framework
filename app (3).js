62b7680cd0db2617ed7380027c3007aa19128334
Eyajeljeli <jeljelieya11@gmail.com>
2025-01-17 02:39:14 +0100

    Ajout des fichiers du td

---
+++ app (3).js
@@ -0,0 +1,9 @@
+const express=require('express');//importer le module express
+const app= express();//creer une instance d'application express
+const port=8000;
+app.get('/',(req,res) =>{
+    res.send('hello');
+});
+app.listen(port,()=>{
+    console.log( `application sur le port ${port}`)
+});
